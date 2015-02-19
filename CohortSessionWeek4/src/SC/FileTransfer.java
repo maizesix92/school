b34806cf538282cc7dbf54c07c974ad4d57a293c
Marcus <marcus_tan@myail.sutd.edu.sg>
2015-02-19 23:30:37 +0800

    finished prime factor mul and file transfer

---
+++ CohortSessionWeek4/src/SC/FileTransfer.java
@@ -0,0 +1,84 @@
+package SC;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class FileTransfer {
+
+	private static ServerSocket serverSocket;
+	private static ArrayList<Socket> listOfSockets = new ArrayList<Socket>();
+	//	private static ArrayList<PrintWriter> listOfPw = new ArrayList<PrintWriter>();
+	private static ArrayList<BufferedReader> listOfBr = new ArrayList<BufferedReader>();
+	private static ArrayList<Thread> listOfThreads = new ArrayList<Thread>();
+
+
+	public static void main(String[] args) throws Exception {
+		FileTransfer ft = new FileTransfer();
+		ft.getClient();
+
+	}
+
+	public void getClient() throws Exception{
+		serverSocket = new ServerSocket(1986);
+		Thread clientListener = new Thread(new Runnable() {
+			private int ID = 0;
+			@Override
+			public void run() {
+				while(true){
+					try {
+
+						Socket clientSocket = serverSocket.accept();
+						ID++;
+						PrintWriter pw = new PrintWriter(clientSocket.getOutputStream(), true);
+						BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+						new Thread(new Runnable(){
+							final int threadID = ID;
+							@Override
+							public void run(){
+								FileWriter fwToFile = null;
+								try {
+									fwToFile = new FileWriter(String.format("C:\\Users\\User\\Desktop\\FileResult%d.txt", threadID), true);
+
+								} catch (IOException e1) {
+									e1.printStackTrace();
+								}
+								String textFromFile;
+								try {
+									while ((textFromFile = br.readLine()) != null){
+										// 1) Readline to get string
+										// 2) Send a feedback to client
+										// 3) Write to file
+
+										pw.println("Received");
+										pw.flush();
+										fwToFile.write(textFromFile);
+										fwToFile.flush();
+
+									}
+									fwToFile.close();
+								} catch (IOException e) {
+									e.printStackTrace();
+								}
+							}
+						}).start();
+						//						listOfSockets.add(clientSocket);
+
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+
+				}
+
+			}
+		});
+		clientListener.start();
+	}
+}
---
+++ CohortSessionWeek4/src/SC/FileTransferClient.java
@@ -0,0 +1,64 @@
+package SC;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+
+public class FileTransferClient {
+
+
+	public static void main(String[] args) throws Exception {
+		// Names of the different text files to pass to server
+		String textName = "File1_1.txt";
+//		String textName = "File1_2.txt";
+//		String textName = "File1_3.txt";
+//		String textName = "File1_4.txt";
+//		String textName = "File1_5.txt";
+//		String textName = "File1_6.txt";
+//		String textName = "File1_7.txt";
+//		String textName = "File1_8.txt";
+		String hostName = "127.0.0.1";
+		int portNumber = 1986;
+		// Client stuff
+		Socket echoSocket = new Socket(hostName, portNumber);
+		PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+		BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+		try{
+			
+			// Reading from text file
+			FileReader textFile = new FileReader("D:\\SUTD\\Term 5\\Software Construction\\Week 4\\TextFiles\\" + textName); // Change this directory as according to respective computer
+			BufferedReader stdIn = new BufferedReader(textFile);
+			String userInput;
+
+			while ((userInput = stdIn.readLine())!= null){
+				out.println(userInput);
+				out.flush();
+				try{
+					echoSocket.setSoTimeout(5000);
+
+					String acknowledgement = in.readLine();
+//					if (acknowledgement.equals("Received")){
+//						continue;
+//					}
+				}catch(SocketTimeoutException e){
+					// re-transmitting 
+					
+					out.println(userInput);
+					out.flush();
+					continue;
+				}
+			}
+			out.close();
+			in.close();
+			stdIn.close();
+			echoSocket.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
