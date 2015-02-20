3ec93ff963b0542813bcef1f0ea289003fdbdb97
maizesix92 <marcustan03@yahoo.com.sg>
2015-02-20 15:39:21 +0800

    blah blah

--- CohortSessionWeek4/src/SC/ChatRoomClient.java
+++ CohortSessionWeek4/src/SC/ChatRoomClient.java
@@ -11,29 +11,37 @@ public class ChatRoomClient {
 
 	public static void main(String[] args) throws Exception {
 		String hostName = "127.0.0.1";
+		Socket echoSocket = null;
+		PrintWriter out = null;
+		BufferedReader in = null;
+		BufferedReader stdIn = null;
 		int portNumber = 1992;
-
-		Socket echoSocket = new Socket(hostName, portNumber);
-		PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
-		BufferedReader in =
-				new BufferedReader(
-						new InputStreamReader(echoSocket.getInputStream()));
-		BufferedReader stdIn =
-				new BufferedReader(
-						new InputStreamReader(System.in));
-		String userInput;
-		System.out.println(in.readLine());
-		do {
-			System.out.println("Type your message: ");
-			userInput = stdIn.readLine();
-			out.println(userInput);
-			out.flush();
-		} while (!userInput.equals("bye"));
-
-		echoSocket.close();
-		in.close();
-		out.close();
-		stdIn.close();           
+		try{
+			echoSocket = new Socket(hostName, portNumber);
+			out = new PrintWriter(echoSocket.getOutputStream(), true);
+			in = new BufferedReader(
+							new InputStreamReader(echoSocket.getInputStream()));
+			stdIn =
+					new BufferedReader(
+							new InputStreamReader(System.in));
+			String userInput;
+			System.out.println(in.readLine());
+			do {
+				System.out.println("Type your message: ");
+				userInput = stdIn.readLine();
+				out.println(userInput);
+				out.flush();
+			} while (!userInput.equals("bye"));
+			echoSocket.close();
+			in.close();
+			out.close();
+			stdIn.close();        
+		}catch(Exception e){
+			echoSocket.close();
+			in.close();
+			out.close();
+			stdIn.close();        
+		}    
 
 	}
 
